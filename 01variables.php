6be20ab5d30bf7dc28783b807afe83a76e6a62c3
daniel220404 <danielestebancarsan@gmail.com>
2022-02-08 14:30:38 -0500

    variables en php

--- 01variables.php
+++ 01variables.php
@@ -2,3 +2,18 @@
 $numero=50;
 $nombre="mauro";
 $bool=true;
+
+$fecha = date('m/d/Y');
+
+$d=rand(-1,-100);
+//echo "el valor aleatorio es igual a ".$d;
+
+$apresente =2022;
+$anacimiento =2004;
+$edad = $apresente - $anacimiento;
+
+echo "segun el año de nacimiento ".$anacimiento." "."su edad es: ". $edad. "anos"
+
+
+
+?>
