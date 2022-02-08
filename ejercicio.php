1a56897ba92df5dcffd3bd4baed402db3249ce43
daniel220404 <danielestebancarsan@gmail.com>
2022-02-08 15:00:47 -0500

    mu

--- ejercicio.php
+++ ejercicio.php
@@ -4,4 +4,4 @@ $cantDolar = rand( 1 , 100);
 $conversion = dolar * $cantDolar;
 
 echo "segun la cantidad de dolares: ".$cantDolar."<br>";
-echo "usted tiene en pesos colombianos la suma de $conversion;
+echo "usted tiene en pesos colombianos la suma de $ " .$conversion;
