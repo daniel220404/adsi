f000a635e522684b7c7d66d0ebdc827d37c1e4e9
daniel220404 <danielestebancarsan@gmail.com>
2022-02-22 13:57:25 -0500

    hi

---
+++ ejemplo.php
@@ -0,0 +1,11 @@
+<?php
+//algoritmo en php para saber si es par o impar
+$numero - rand(1 , 50);
+$validar - $numero%2;
+if ($validar === 0) {
+    echo "el numero a validar es: ",$numero, "<br>";
+    echo "el numero es par";
+}else
+    echo "el numero a validar es: ",$numero, "<br>";
+    echo "el numero es impar";
+}
