22b62140045d52ccbdfabe2a1aa28ff018ad7679
daniel220404 <danielestebancarsan@gmail.com>
2022-02-22 16:00:40 -0500

    jzn

--- ejemplo2.php
+++ ejemplo2.php
@@ -2,8 +2,7 @@
 //algoritmo en php para saber si es par o impar
 $numero = rand(-3,3);
 if ($numero > 0){
-    echo "el valor $numero es ➕";
-}elseif($numero<0){
+}elseif(|$numero<0){
     echo "el valor $numero es ➖";
 }else{
     echo "el valor $numero es equivalente a CERO";
